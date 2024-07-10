cbed617123857674194cd2ce1b943423515cd78d
TheDrawingCoding-Gamer <bulbyvr@gmail.com>
2024-07-10 15:54:31 -0400

    remove sound adjustment

--- source/Main.hx
+++ source/Main.hx
@@ -5,6 +5,7 @@ import haxe.ui.Toolkit;
 import openfl.display.FPS;
 import openfl.display.Sprite;
 import flixel.FlxG;
+// import flixel.input.keyboard.FlxKey;
 
 class Main extends Sprite
 {
@@ -14,6 +15,9 @@ class Main extends Sprite
 		Toolkit.init();
 		Toolkit.theme = "dark";
 		addChild(new FlxGame(0, 0, PlayState, 60, 60, true));
+		FlxG.sound.muteKeys = null;
+		FlxG.sound.volumeUpKeys = null;
+		FlxG.sound.volumeDownKeys = null;
 		// addChild(new FPS(10, 3, 0xFFFFFF));
 	}
 }
